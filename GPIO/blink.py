4105a39db26bd7a2b085a6327285f17509cb4d5c
eyllanesc <e.yllanescucho@gmail.com>
2017-05-03 11:46:32 -0500

    update

---
+++ GPIO/blink.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+led = 18  #GPIO18
+delay = 1 #one second
+GPIO.setwarnings(False) #disable warnings
+GPIO.setmode(GPIO.BCM) # mode BCM or Board
+GPIO.setup(led, GPIO.OUT) # input or output
+while True:
+	GPIO.output(led, True)
+	time.sleep(delay)
+	GPIO.output(led, False)
+	time.sleep(delay)
---
+++ GPIO/pwm.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+led = 18  #GPIO18
+GPIO.setwarnings(False) #disable warnings
+GPIO.setmode(GPIO.BCM) # mode BCM or Board
+GPIO.setup(led, GPIO.OUT) # input or output
+pwm_led = GPIO.PWM(led, 500)
+pwm_led.start(100)
+while True:
+	duty_s = raw_input("Enter Brightness (0 to 100): ")
+	duty = int(duty_s)
+	pwm_led.ChangeDutyCycle(duty)
