f6b5a09ecd026fa18e61218287710aa38b1d2c26
eyllanesc <e.yllanescucho@gmail.com>
2017-05-03 11:48:30 -0500

    update

---
+++ switch1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+switch = 18  #GPIO18
+delay = 0.2 #0.2 seconds
+GPIO.setwarnings(False) #disable warnings
+GPIO.setmode(GPIO.BCM) # mode BCM or Board
+GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP) # input or output
+while True:
+	input_state = GPIO.input(switch)
+	if input_state == False:
+		print("Button Pressed")
+	time.sleep(delay)
